f439d9e98dd323e9fba1ca115c931095b6242676
PRMeesYT <57196957+PRMeesYT@users.noreply.github.com>
2023-11-08 12:08:04 +0100

    Geluid en start menu

---
+++ Assets/Fade/Scripts/FadeManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+    #region Instance
+    public static FadeManager Instance;
+    #endregion
+
+    [Header("Fade references")]
+    [SerializeField] private CanvasGroup fadeGroup;
+    [SerializeField] private Image fadeImage;
+
+    [Header("Start Fade")]
+    [SerializeField] private bool startWithFade = true;
+    [SerializeField] private float startFadeTime = 1.5f;
+    [SerializeField] private Color startFadeColor = Color.black;
+
+    private void Awake()
+    {
+        // Set the static instance directly
+        Instance = this;
+
+        // Keep this alive throughout the game
+        DontDestroyOnLoad(this.gameObject);
+
+        if (startWithFade)
+        {
+            fadeGroup.alpha = 1;
+            FadeIn(startFadeTime, startFadeColor);
+        }
+        else
+        {
+            fadeGroup.alpha = 0;
+        }
+    }
+
+    public void FadeOut(float transitionTime)
+    {
+        // Re-use last color, no action at the end
+        FadeOut(transitionTime, fadeImage.color, null);
+    }
+    public void FadeOut(float transitionTime, Color fadeColor)
+    {
+        // Apply new color, no action at the end
+        FadeOut(transitionTime, fadeColor, null);
+    }
+    public void FadeOut(float transitionTime, Color fadeColor, Action func)
+    {
+        fadeImage.color = fadeColor;
+        StartCoroutine(UpdateFadeOut(transitionTime,func));
+    }
+
+    public void FadeIn(float transitionTime)
+    {
+        // Re-use last color, no action at the end
+        FadeIn(transitionTime, fadeImage.color, null);
+    }
+    public void FadeIn(float transitionTime, Color fadeColor)
+    {
+        // Apply new color, no action at the end
+        FadeIn(transitionTime, fadeColor, null);
+    }
+    public void FadeIn(float transitionTime, Color fadeColor, Action func)
+    {
+        fadeImage.color = fadeColor;
+        StartCoroutine(UpdateFadeIn(transitionTime, func));
+    }
+
+    private IEnumerator UpdateFadeOut(float transitionTime, Action func)
+    {
+        float t = 0.0f;
+
+        for (t = 0.0f; t <= 1; t += Time.deltaTime/transitionTime)
+        {
+            fadeGroup.alpha = t;
+            yield return null;
+        }
+
+        fadeGroup.alpha = 1;
+        func?.Invoke();
+    }
+    private IEnumerator UpdateFadeIn(float transitionTime, Action func)
+    {
+        float t = 0.0f;
+
+        for (t = 0.0f; t <= 1; t += Time.deltaTime / transitionTime)
+        {
+            fadeGroup.alpha = 1 - t;
+            yield return null;
+        }
+
+        fadeGroup.alpha = 0;
+        func?.Invoke();
+    }
+}
--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -5,16 +5,28 @@ public class StartManager : MonoBehaviour
 {
     [SerializeField] private GameObject settingsMenu;
     [SerializeField] private GameObject mainButtons;
+    [SerializeField] private GameObject soloMenu;
+
+    public AudioClip buttonClickSFX;
+    public AudioClip music1;
+    public AudioClip music2;
+
     private bool settingsOpen;
+    private bool soloOpen;
+
     void Start()
     {
+        AudioManager.Instance.PlayMusicWithCrossFade(music1, 3.0f);
         settingsMenu.SetActive(false);
         mainButtons.SetActive(true);
+        soloMenu.SetActive(false);
         settingsOpen = false;
+        soloOpen = false;
     }
 
     public void SettingsMenu()
     {
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
         if (settingsOpen)
         {
             mainButtons.SetActive(true);
@@ -31,16 +43,55 @@ public class StartManager : MonoBehaviour
 
     public void SoloMode()
     {
-        SceneManager.LoadScene("SoloScene");
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
+
+        if (soloOpen)
+        {
+            mainButtons.SetActive(true);
+            soloMenu.SetActive(false);
+            soloOpen = false;
+        }
+        else if (!soloOpen)
+        {
+            mainButtons.SetActive(false);
+            soloMenu.SetActive(true);
+            soloOpen = true;
+        }
+    }
+
+    public void LoadMap1()
+    {
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
+        SceneManager.LoadScene("SoloMap1");
+    }
+
+    public void LoadMap2()
+    {
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
+        SceneManager.LoadScene("SoloMap2");
+    }
+
+    public void LoadMap3()
+    {
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
+        SceneManager.LoadScene("SoloMap3");
+    }
+
+    public void LoadMap4()
+    {
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
+        SceneManager.LoadScene("SoloMap4");
     }
 
     public void VersusMode()
     {
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
         SceneManager.LoadScene("VersusScene");
     }
 
     public void QuitGame()
     {
+        AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
         Application.Quit();
     }
 }
